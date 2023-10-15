3fe5c060cbdd932beca4578323fbd8e2ddf6e67f
Timofei Novikov <tictacnumt@gmail.com>
2023-10-15 12:38:51 +0300

    lab is ready

--- lab_1.s
+++ lab_1.s
@@ -8,23 +8,17 @@ main:
     LDR R1, =0x42420308             // Address of bitband RCC_APB2ENR
     STR R0, [R1]                    // Enable port A clocking
     // port A clocking enabled
- 
 
     LDR R0, =0x11111114             // Configure PA1 - PA7 in output push-pull mode
     LDR R1, =0x40010800             // Address of GPIOA_CRL
-    LDR R2, [R1]                    // Load GPIOA_CRL into R2
-    BFI R2, R0, #0x0, #28           // Insert configuration into GPIOA_CRL  (insert first 28 bits of R0 into R2 begining from 0x0 bit)
-    STR R2, [R1]                    // Write new configuration to the GPIOA_CRL
+    STR R0, [R1]                    // Write new configuration to the GPIOA_CRL
     // GPIOA 1 - 7 configured as output push-pull, GPIOA_0 - is User button
 
 begining:
-    LDR R0, =0x40010814             // BRR
-    LDR R1, =0xFE                   // Clear GPIOA 1 - 7
-    STR R0, [R1]                    // Execute cleaning
-    // GPIOA 1 - 7 are reseted
     LDR R4, =0x42210100             // Bit-banding adress of the PA0 button (0 bit of GPIOA_IDR)
-    MOV R5, #0                      // Counter
-    B indicate_0                    // Set indicator to zero state
+    MOV R5, #0                      // Reset counter
+    LDR R6, =0xFFFFF                // Set delay
+    B wait                          // Set indicator to zero state
     
 read_button:
     LDR R2, [R4]                    // Reading of the button state
@@ -37,20 +31,18 @@ increment:
 
     LDR R6, =0xFFFFF                 // set counter
 wait:
-    CMP R6, #0x0                    // While counter R0 > 0
+    CMP R6, #0x0                    // While delay_counter R6 > 0
     BEQ load_num                    // If R6 == 0 increment 7 segment indicator
-    SUB R6, #0x1                    // Decrement counter
+    SUB R6, #0x1                    // Decrement delay_counter
     B wait                          // Return to the wait
 load_num:
-    LDR R0, =0x40010814             // Adress of BRR
-    LDR R1, =0xFE                   // Clear GPIOA 1 - 7
-    STR R0, [R1]                    // Execute cleaning
-    LDR R8, =0x4001080C              // check ODR
-    LDR R9, [R8]
-    // 7 segment indicator is empty
 
-
-    LDR R0, =0x40010810             // Address of GPIOA_BSRR 
+    LDR R0, =0x4001080C             // Address of GPIOA_ODR
+    LDR R1, [R0]                    // Read GPIOA_ODR
+    LDR R2, =0xFF01                 // Mask where 1 - 7 bits are 0, other are 1
+    AND R1, R1, R2                  // Clear 1 - 7 pin (reset)
+    STR R1, [R0]                    // Write Cleared ODR
+    LDR R0, =0x40010810             // Address of GPIOA_BSRR
 
     TBB.W [PC, R5]                  // R5 - counter
 branchtable:
@@ -69,9 +61,9 @@ indicate_0:
     LDR R1, =0xEE                   // 0xEE = simbol 0 in 7segment indicator
     STR R1, [R0]                    // Write to BSRR
     B read_button
-    // 7 segment indicator is set to show '1'
+    // 7 segment indicator is set to show '0'
 indicate_1:
-    LDR R1, =0x18                   // 0x18 = simbol 1 in 7segment indicator
+    LDR R1, =0x88                   // 0x88 = simbol 1 in 7segment indicator
     STR R1, [R0]                    // Write to BSRR
     B read_button
     // 7 segment indicator is set to show '1'
@@ -96,7 +88,7 @@ indicate_5:
      B read_button
      // 7 segment indicator is set to show '5'
 indicate_6:
-     LDR R1, =0xBE                   // 0xBE = simbol 6 in 7segment indicator
+     LDR R1, =0xF6                   // 0xF6 = simbol 6 in 7segment indicator
      STR R1, [R0]                    // Write to BSRR
      B read_button
      // 7 segment indicator is set to show '6'
@@ -115,4 +107,15 @@ indicate_9:
      STR R1, [R0]                    // Write to BSRR
      B read_button
      // 7 segment indicator is set to show '9'
-     
+
+/* Scheme of connection
+ ----2----
+|         |         PA1 - PA7
+1         3
+|         |         ON  - high voltage
+ ----4----          OFF - low voltage
+|         |         
+5         7
+|         |
+ ----6----     
+*/
