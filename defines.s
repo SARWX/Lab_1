9e2b5f921986449cfaa1715b02f1004f87426ab4
Timofei Novikov <tictacnumt@gmail.com>
2023-10-22 02:00:11 +0300

    new system with defines

---
+++ defines.s
@@ -0,0 +1,14 @@
+.set GPIOA, 0x40010800
+.set GPIOB, 0x40010C00
+.set GPIOC, 0x40011000
+.set GPIOD, 0x40011400
+.set GPIOE, 0x40011800
+
+.set RCC, 0x40021000
+
+.set APB2ENR, 0x18
+.set CRL, 0x00
+.set BRR, 0x14
+.set IDR, 0x08
+.set ODR, 0x0C
+.set BSRR, 0x10
--- lab_1.s
+++ lab_1.s
@@ -1,21 +1,28 @@
+.include "defines.s"
 .globl main
 .text
 .thumb
 .syntax unified
 
+.set BB_RCC_APB2ENR, (0x42000000 + (RCC - 0x40000000 + APB2ENR) * 32)      // Bit banding adress of the RCCAPB2ENR (zero bit)
+.set BB_GPIOA_IDR, (0x42000000 + (GPIOA - 0x40000000 + IDR) * 32)           // Bit-banding adress of the GPIOA_IDR (zero bit)
+.set GPIOA_CONF_7_SEGMENT, (1*0x10000000 + 1*0x1000000 + 1*0x100000 + 1*0x10000 + 1*0x1000 + 1*0x100 + 1*0x10 + 4) 
+// PA0 - floating input; PA1 ... PA7 - output push-pull
+
 main:
     MOV R0, #1                      // Enable port A clocking
-    LDR R1, =0x42420308             // Address of bitband RCC_APB2ENR
+    LDR R1, =(BB_RCC_APB2ENR+(2*4)) // Bit banding adress of second bit RCCAPB2ENR
     STR R0, [R1]                    // Enable port A clocking
     // port A clocking enabled
 
-    LDR R0, =0x11111114             // Configure PA1 - PA7 in output push-pull mode
-    LDR R1, =0x40010800             // Address of GPIOA_CRL
+    LDR R0, =(GPIOA_CONF_7_SEGMENT) // Configure PA1 - PA7 in output push-pull mode
+    LDR R1, =(GPIOA + CRL)          // Address of GPIOA_CRL
+    
     STR R0, [R1]                    // Write new configuration to the GPIOA_CRL
     // GPIOA 1 - 7 configured as output push-pull, GPIOA_0 - is User button
 
 begining:
-    LDR R4, =0x42210100             // Bit-banding adress of the PA0 button (0 bit of GPIOA_IDR)
+    LDR R4, =BB_GPIOA_IDR           // Bit-banding adress of the PA0 button (0 bit of GPIOA_IDR)
     MOV R5, #0                      // Reset counter
     LDR R6, =0xFFFFF                // Set delay
     B wait                          // Set indicator to zero state
@@ -37,12 +44,12 @@ wait:
     B wait                          // Return to the wait
 load_num:
 
-    LDR R0, =0x4001080C             // Address of GPIOA_ODR
+    LDR R0, = GPIOA + ODR           // Address of GPIOA_ODR
     LDR R1, [R0]                    // Read GPIOA_ODR
     LDR R2, =0xFF01                 // Mask where 1 - 7 bits are 0, other are 1
     AND R1, R1, R2                  // Clear 1 - 7 pin (reset)
     STR R1, [R0]                    // Write Cleared ODR
-    LDR R0, =0x40010810             // Address of GPIOA_BSRR
+    LDR R0, =(GPIOA + BSRR)         // Address of GPIOA_BSRR
 
     TBB.W [PC, R5]                  // R5 - counter
 branchtable:
